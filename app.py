ea5f8da4eea76a13d70248eaafcf2e182604fefd
iluffy2050 <mangoisafruitilike@gmail.com>
2025-09-13 19:50:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,39 +8,38 @@ app = Flask(__name__, template_folder='.')
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
 
-users = {}
+users = {}  # sid -> {'username':..., 'pfp':...}
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# ... rest of your SocketIO handlers ...
-
 @socketio.on('join')
 def handle_join(data):
-    username = data['username']
-    pfp = data['pfp']
+    username = data.get('username')
+    pfp = data.get('pfp', '')
     users[request.sid] = {'username': username, 'pfp': pfp}
     emit('user_joined', {'username': username, 'pfp': pfp}, broadcast=True)
 
 @socketio.on('send_message')
 def handle_message(data):
-    username = users[request.sid]['username']
-    pfp = users[request.sid]['pfp']
-    message = data['message']
-    emit('receive_message', {'username': username, 'pfp': pfp, 'message': message}, broadcast=True)
+    user = users.get(request.sid)
+    if not user: return
+    message = data.get('message')
+    emit('receive_message', {'username': user['username'], 'pfp': user['pfp'], 'message': message}, broadcast=True)
 
 @socketio.on('typing')
 def handle_typing(data):
-    username = users[request.sid]['username']
-    emit('user_typing', {'username': username}, broadcast=True, include_self=False)
+    user = users.get(request.sid)
+    if not user: return
+    emit('user_typing', {'username': user['username']}, broadcast=True, include_self=False)
 
 @socketio.on('disconnect')
 def handle_disconnect():
-    if request.sid in users:
-        username = users[request.sid]['username']
-        emit('user_left', {'username': username}, broadcast=True)
-        users.pop(request.sid)
+    user = users.pop(request.sid, None)
+    if user:
+        emit('user_left', {'username': user['username']}, broadcast=True)
 
+# For local testing only:
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
