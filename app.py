fbeab94d5bc4cc433b3afa1cebb5b07f86153dba
iluffy2050 <mangoisafruitilike@gmail.com>
2025-09-14 10:20:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,60 +3,42 @@ import time
 
 app = Flask(__name__, template_folder='.')
 
-# Store messages in memory
-messages = []  # {'username','pfp','message','timestamp','seen':set()}
-typing_users = set()
+messages = []   # [{username, pfp, message, seen_by}]
+typing_user = None
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/send', methods=['POST'])
-def send():
-    data = request.json
-    username = data.get('username')
-    pfp = data.get('pfp','')
-    message = data.get('message')
-    if username and message:
-        messages.append({
-            'username': username,
-            'pfp': pfp,
-            'message': message,
-            'timestamp': time.time(),
-            'seen': set()  # track who has seen
-        })
-    return jsonify({'status':'ok'})
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    data=request.get_json()
+    msg={
+        "username":data['username'],
+        "pfp":data['pfp'],
+        "message":data['message'],
+        "seen_by": None,
+        "timestamp": time.time()
+    }
+    messages.append(msg)
+    return {"status":"ok"}
 
 @app.route('/get_messages', methods=['POST'])
 def get_messages():
-    user = request.json.get('username')
-    # Mark all messages as seen by this user
-    for msg in messages:
-        msg['seen'].add(user)
-    # Return last 50 messages
-    data = []
-    for msg in messages[-50:]:
-        data.append({
-            'username': msg['username'],
-            'pfp': msg['pfp'],
-            'message': msg['message'],
-            'seen_count': len(msg['seen'])
-        })
-    return jsonify(data)
+    global typing_user
+    data=request.get_json()
+    user=data['username']
+    # mark last message as seen by this user
+    if messages:
+        messages[-1]["seen_by"]=user
+    return jsonify({"messages":messages,"typing":typing_user})
 
 @app.route('/typing', methods=['POST'])
 def typing():
-    user = request.json.get('username')
-    if user:
-        typing_users.add(user)
-    return jsonify({'status':'ok'})
+    global typing_user
+    data=request.get_json()
+    typing_user=data['username']
+    return {"status":"ok"}
 
-@app.route('/get_typing', methods=['GET'])
-def get_typing():
-    # Return typing users and clear set
-    users = list(typing_users)
-    typing_users.clear()
-    return jsonify(users)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=5000)
